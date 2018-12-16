5dbf54e9f31743419c58ccc44cad14c8bb142d8c
brunogvaz <brunogvaz@hotmail.com>
2018-12-15 23:59:44 -0200

    first

---
+++ OdeToFood/IGreeter.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace OdeToFood
+{
+    public interface IGreeter
+    {
+        string GetMessageofDay();
+    }
+    public class Greeter : IGreeter
+    {
+        private IConfiguration _configuration;
+
+        public Greeter(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public string GetMessageofDay()
+        {
+            return _configuration["Greeting"];
+        }
+    }
+
+}
